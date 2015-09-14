39a781d62a87bda69d340132b5caf1e2aab5962a
LEO <120039015@qq.com>
2015-09-14 15:31:24 -0400

    be able to get headers info

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
-var app        = express();
+var app = express();
 
 // configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080; // set our port
@@ -16,13 +18,12 @@ var port = process.env.PORT || 8080; // set our port
 // Redis Connection
 // =============================================================================
 var redis = require("redis"),
-client = redis.createClient();
+    client = redis.createClient();
 
 // if you'd like to select database 3, instead of 0 (default), call
-client.select(0, function() {
-});
+client.select(0, function() {});
 
-client.on("error", function (err) {
+client.on("error", function(err) {
     console.log("Error " + err);
 });
 
@@ -34,80 +35,121 @@ var router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	console.log('Something is happening.');
-	next();
+    console.log('Something is happening.');
+    next();
 });
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
 // on routes that end in /employees
 // ----------------------------------------------------
 router.route('/employees')
-
-	//create employee
-	.post(function(req, res) {	
-		client.hset("employee", req.query.key, req.query.value, function (err, obj) {
-			if (err) {
-				res.send(err);
-			} else {
-				res.send("employee created");
-			}
-		});
-	})
+    //create employee
+    .post(function(req, res) {
+        client.hset("employee", req.query.key, req.query.value, function(err, obj) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("employee created");
+            }
+        });
+    })
 
 	//get all employees
 	.get(function(req, res) {
-		client.hgetall("employee", function (err, obj) {
-			if (err) {
-				res.send(err);
-			}
-		});
+	    client.hgetall("employee", function(err, obj) {
+	        if (err) {
+	            res.send(err);
+	        }
+	    });
 	});
+
 
 router.route('/employees/:key')
 
 	//get employee with key
 	.get(function(req, res) {
-		client.hget("employee", req.params.key, function(err, obj){
-			if (err) {
-			    res.send(err);
-			} else {
-				res.json(obj);
-			}
-		});
+	    client.hget("employee", req.params.key, function(err, obj) {
+	        if (err) {
+	            res.send(err);
+	        } else {
+	            res.json(obj);
+	        }
+	    });
 	})
 
 	// update employee value with key
 	.put(function(req, res) {
-		client.hset("employee", req.params.key, req.query.value, function (err, obj) {
-			if (err) {
-				res.send(err);
-			} else {
-				res.send("description updated");
-			}
-		});
+	    client.hset("employee", req.params.key, req.query.value, function(err, obj) {
+	        if (err) {
+	            res.send(err);
+	        } else {
+	            res.send("description updated");
+	        }
+	    });
 	})
 
 	// delete the employee with key
 	.delete(function(req, res) {
-		client.hdel("employee", req.params.key, function(err, obj){
-			if (err) {
-				res.send(err);
-			} else {
-				res.send("employee deleted")
-			}
-			//res.json(obj);
-		});
+	    client.hdel("employee", req.params.key, function(err, obj) {
+	        if (err) {
+	            res.send(err);
+	        } else {
+	            res.send("employee deleted")
+	        }
+	        //res.json(obj);
+	    });
+	});
+
+router.route('/tokens/:key')
+	//get employee with key
+	.get(function(req, res) {
+  //    var tokenBoolean = isTokenActive(req.params.key, "123");
+  //   	if (tokenBoolean) {
+  //   		console.log("second yes");
+  //   		res.send("active");
+  //   	} else {
+  //   		console.log("second no");
+  //   		res.send("non-active");
+  //   	}	
+  		console.log(req.header);
+  		client.hget("token", req.params.key, function(err, obj) {
+	    	if (obj == "123") {
+	    		res.send("active");
+	    	} else {
+	    		res.send("non-active");
+	    	}
+    	});
+
+	})
+
+	.post(function(req, res){
+		console.log("post request");
+		console.log(req.headers.token);
 	});
 
 
-function isTokenActive (var tokenKey, var tokenValue) {
-
+function isTokenActive(tokenKey, tokenValue) {
+    client.hget("token", tokenKey, function(err, obj) {
+    	if (obj == tokenValue) {
+    		console.log("yes");
+    		return true;
+    	} else {
+    		console.log("no");
+    		return false;
+    	}
+    });
 }
 
+function getTokenValue() {
+
+}
 
 // REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
